47e812a7617469f8488996d6ba9a9fa7e54be126
kl <kevin@oneliferemains.com>
2019-10-03 19:37:56 +0200

    added map util

---
+++ Halpers/Math.cs
@@ -0,0 +1,10 @@
+﻿namespace awkward
+{
+  public class Math
+  {
+    public static float map(float val, float sourceMin, float sourceMax, float targetMin, float targetMax)
+    {
+      return targetMin + (targetMax-targetMin) * ( (val-sourceMin) / (sourceMax-sourceMin) );
+    }
+  }
+}
