6b1508d4f826157e0b3e6faa513aa96be1d3d203
ab <andre.berlemont@gmail.com>
2019-10-11 16:48:06 +0200

    shuffle

---
+++ Halpers/HalperArrays.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class HalperArrays 
+{
+
+  /// <summary>
+  /// shuffle list of Object
+  /// </summary>
+  static public List<T> shuffle<T>(this List<T> list)
+  {
+    for (int i = 0; i < list.Count; i++)
+    {
+      T temp = list[i];
+      int randomIndex = Random.Range(i, list.Count);
+      list[i] = list[randomIndex];
+      list[randomIndex] = temp;
+    }
+    return list;
+  }
+
+}
