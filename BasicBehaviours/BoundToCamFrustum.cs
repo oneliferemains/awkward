9ac978cc55ffb469c7c36dddc27140e433904abc
kevin <kevin@oneliferemains.com>
2019-10-16 17:47:09 +0200

    BoundToCam

---
+++ BasicBehaviours/BoundToCamFrustum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// keep a transform inside cam frustum
+// uses the MainCamera if none provided
+public class BoundToCamFrustum : MonoBehaviour
+{
+    public Camera cam;
+    
+    void Awake()
+    {
+      if(cam == null)
+      {
+        cam = Camera.main;
+      }
+    }
+    
+    void LateUpdate()
+    {
+      Vector3 viewportPos = cam.WorldToViewportPoint(transform.position); 
+      viewportPos.x = Mathf.Clamp01(viewportPos.x);
+      viewportPos.y = Mathf.Clamp01(viewportPos.y);
+      transform.position = cam.ViewportToWorldPoint(viewportPos);
+    }
+}
