9d4190545ae26233b1fa7c1346700f510987c6da
ab <andre.berlemont@gmail.com>
2020-04-03 15:44:37 +0200

    added basic movement for orbital

---
+++ Input/BasicMouseOrbiter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMouseOrbiter : MonoBehaviour
+{
+  public float deltaScale = 0.66f;
+
+  Vector2 _dlt;
+
+  private void Start()
+  {
+    _dlt = Input.mousePosition;
+  }
+
+  void Update()
+  {
+    Vector2 mPos = Input.mousePosition;
+    Vector2 dlt = mPos - _dlt;
+
+    //Debug.Log(dlt);
+
+    Vector2 solvedDelta = dlt * deltaScale;
+
+    if(solvedDelta.sqrMagnitude > 0.01f)
+    {
+      solvedDelta *= Time.deltaTime;
+
+      transform.Rotate(Vector2.up * solvedDelta.x, Space.World);
+      transform.Rotate(Vector2.right * -solvedDelta.y, Space.Self);
+    }
+
+    _dlt = mPos; // buff
+  }
+}
---
+++ Input/BasicZQSDMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicZQSDMovement : MonoBehaviour
+{
+  public bool physic = false;
+
+  Rigidbody _rigid;
+
+  public float speed = 10f;
+
+  [SerializeField][ReadOnly]
+  Vector3 dir = Vector3.zero;
+
+  private void Start()
+  {
+    if (physic)
+    {
+      _rigid = GetComponent<Rigidbody>();
+    }
+  }
+
+  private void FixedUpdate()
+  {
+    if (_rigid == null) return;
+
+    Vector3 vel = Vector3.zero;
+
+    if(dir.sqrMagnitude > 0f)
+    {
+      vel = (transform.TransformPoint(dir) - transform.position).normalized * speed;
+    }
+
+    _rigid.velocity = vel;
+  }
+
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.Z)) dir.z = 1f;
+    else if (Input.GetKey(KeyCode.S)) dir.z = -1f;
+    else dir.z = 0f;
+
+    if (Input.GetKey(KeyCode.Q)) dir.x = -1f;
+    else if (Input.GetKey(KeyCode.D)) dir.x = 1f;
+    else dir.x = 0f;
+
+    if (_rigid == null) transform.Translate(dir * speed * Time.deltaTime, Space.Self);
+  }
+}
