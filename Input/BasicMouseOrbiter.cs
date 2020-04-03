24ccf99548961b1cde32bbde5546c385ace6405a
ab <andre.berlemont@gmail.com>
2020-04-03 16:17:28 +0200

    mouse orbiter

--- Input/BasicMouseOrbiter.cs
+++ Input/BasicMouseOrbiter.cs
@@ -4,30 +4,82 @@ using UnityEngine;
 
 public class BasicMouseOrbiter : MonoBehaviour
 {
-  public float deltaScale = 0.66f;
+  float deltaScaleX = 5f;
+  float deltaScaleY = 3f;
 
   Vector2 _dlt;
 
+  float angleX = 0f;
+  float angleY = 0f;
+
   private void Start()
   {
+
     _dlt = Input.mousePosition;
+
+    lockMouse();
+  }
+
+  void lockMouse()
+  {
+
+    Cursor.lockState = CursorLockMode.Confined;
+    Cursor.visible = false;
   }
 
   void Update()
   {
+
+    if(Input.GetMouseButtonDown(0))
+    {
+      lockMouse();
+    }
+
+    if(Input.GetKeyUp(KeyCode.Escape))
+    {
+      Cursor.lockState = CursorLockMode.None;
+      Cursor.visible = true;
+    }
+
     Vector2 mPos = Input.mousePosition;
     Vector2 dlt = mPos - _dlt;
 
     //Debug.Log(dlt);
 
-    Vector2 solvedDelta = dlt * deltaScale;
+    Vector2 solvedDelta;
+    
+    solvedDelta.x = dlt.x * deltaScaleX;
+    solvedDelta.y = dlt.y * deltaScaleY;
 
-    if(solvedDelta.sqrMagnitude > 0.01f)
+    if (solvedDelta.sqrMagnitude > 0.01f)
     {
       solvedDelta *= Time.deltaTime;
 
-      transform.Rotate(Vector2.up * solvedDelta.x, Space.World);
-      transform.Rotate(Vector2.right * -solvedDelta.y, Space.Self);
+      Vector3 euler = Vector3.zero;
+      
+      if(solvedDelta.x != 0f)
+      {
+        angleX += solvedDelta.x; // up
+
+        euler = transform.eulerAngles;
+        euler.y = angleX;
+        transform.eulerAngles = euler;
+      }
+
+      if(solvedDelta.y != 0f)
+      {
+        angleY += solvedDelta.y; // right
+
+        euler = transform.localEulerAngles;
+        euler.x = -angleY;
+
+        euler.x = Mathf.Clamp(euler.x, -75f, 75f);
+
+        transform.localEulerAngles = euler;
+      }
+
+      //transform.Rotate(Vector2.up * solvedDelta.x, Space.World);
+      //transform.Rotate(Vector2.right * -solvedDelta.y, Space.Self);
     }
 
     _dlt = mPos; // buff
