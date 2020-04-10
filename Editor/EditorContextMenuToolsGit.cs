bba9577fcbe913843fea7d7c750a2391a03c0e3a
ab <andre.berlemont@gmail.com>
2020-04-10 11:08:10 +0200

    git tool opening

---
+++ Editor/EditorContextMenuToolsGit.cs
@@ -0,0 +1,146 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+
+public class EditorContextMenuToolsGit : MonoBehaviour
+{
+  //[MenuItem("Assets/git !#&%g")]
+  [MenuItem("Assets/git &%g")]
+  static protected void openGit()
+  {
+    //string fullPath = Path.Combine(Environment.CurrentDirectory, "/YourSubDirectory/yourprogram.exe");
+    //string fullPath = getFolderPathContainingGit(Environment.CurrentDirectory);
+
+    //start a git cmd within project context (../Assets)
+    //Debug.Log("base root git folder : " + fullPath);
+
+    HalperEditor.ClearConsole();
+
+    string fullPath = Environment.CurrentDirectory;
+    Debug.Log(fullPath);
+
+    //Debug.Log("opening gits from "+ fullPath);
+    
+    if (pathHasGitFolder(fullPath))
+    {
+      Debug.Log("current path has git folder : opening");
+      HalperNatives.startCmd("git", "--cd=" + fullPath);
+    }
+
+    //Directory.Exists(fullPath)
+
+    string dirAssets = Path.Combine(fullPath, "Assets");
+    string dirLib = Path.Combine(dirAssets, "lib");
+
+    if(Directory.Exists(dirLib))
+    {
+
+      string[] dirs = Directory.GetDirectories(dirLib);
+      foreach(string dir in dirs)
+      {
+        if (Directory.Exists(dir))
+        {
+          //int lastIndex = file.LastIndexOf('\\');
+          //string nm = file.Substring(file.LastIndexOf('\\'));
+
+          if(pathHasGitFolder(dir))
+          {
+            openGitFolderByPath(dir);
+          }
+          
+          //nm = nm.Substring(1); // remove last \
+          //if (nm.StartsWith("fw")) nm = nm.Substring(2); // remove fw
+
+          //openGitFolderByName(nm);
+        }
+      }
+    }
+
+    //Debug.Log("seaching for other gits");
+    //openGitFolderByName("protoss");
+  }
+  
+  static public void openGitFolderByName(string containsFolderName)
+  {
+    string fullPath = Environment.CurrentDirectory;
+
+    //searcg for protoss git folder and open it
+    string path = getFolderPathContainingGit(fullPath, containsFolderName);
+
+    //Debug.Log("protoss : " + path);
+
+    openGitFolderByPath(path);
+  }
+
+  static public void openGitFolderByPath(string path)
+  {
+
+    if (path.Length <= 0) Debug.LogWarning("no path given ?");
+    else
+    {
+      Debug.Log("exec git command in : "+path);
+      HalperNatives.startCmd("git", "--cd=" + path);
+    }
+
+  }
+
+  static public bool pathHasGitFolder(string basePath)
+  {
+    //Debug.Log(basePath);
+
+    string path;
+
+    //search for a /.git/ folder
+    string[] dirs = Directory.GetDirectories(basePath);
+    for (int i = 0; i < dirs.Length; i++)
+    {
+      path = dirs[i].ToLower();
+      if (path.Contains(".git")) return true;
+    }
+
+    //search for .git file (submodules)
+    string[] files = Directory.GetFiles(basePath);
+    for (int i = 0; i < files.Length; i++)
+    {
+      path = files[i].ToLower();
+      if (path.Contains(".git")) return true;
+    }
+
+    return false;
+  }
+  
+  /// <summary>
+  /// recurcively search for git folder
+  /// </summary>
+  static private string getFolderPathContainingGit(string basePath, string folderName)
+  {
+    //Debug.Log("path : " + basePath);
+
+    folderName = folderName.ToLower();
+    basePath = basePath.ToLower();
+
+    
+    if(HalperIO.isLastFolderInPath(basePath, folderName))
+    {
+      //Debug.Log(basePath);
+
+      if (pathHasGitFolder(basePath)) return basePath;
+    }
+    
+    //this returns FULL PATHs
+    string[] dirs = Directory.GetDirectories(basePath);
+
+    foreach (string dir in dirs)
+    {
+      if (HalperIO.isLastFolderDotFolder(dir)) continue;
+
+      string output = getFolderPathContainingGit(dir, folderName);
+      if (output.Length > 0) return output;
+    }
+    
+    return "";
+  }
+}
+#endif
---
+++ Halpers/HalperIO.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class HalperIO
+{
+  static public FileStream getStream(string path)
+  {
+    if (!File.Exists(path)) File.Create(path);
+    return File.OpenWrite(path);
+  }
+    
+  static public List<string> getLinesOfFile(string path)
+  {
+    throw new NotImplementedException("todo");
+
+    /*
+    FileStream fs = getStream(path);
+    //fs.
+    //string[] splitted = patternFile.text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+    return null;
+    */
+  }
+
+  static public string getFolderPathByName(string basePath, string folderName)
+  {
+    //this returns FULL PATHs
+    string[] dirs = Directory.GetDirectories(basePath);
+    
+    foreach (string dir in dirs)
+    {
+      if (dir.ToLower().EndsWith(folderName.ToLower())) return dir;
+      string output = getFolderPathByName(dir, folderName);
+      if (output.Length > 0) return output;
+    }
+
+    return "";
+  }
+
+
+
+
+  /// <summary>
+  /// returns true if last folder in path is the same as folderName param
+  /// </summary>
+  static public bool isLastFolderInPath(string path, string folderName)
+  {
+    path = cleanPathFromFile(path);
+    return path.EndsWith(folderName);
+  }
+
+  /// <summary>
+  /// is the last folder in path is a folder starting with a '.' char (ex : git related)
+  /// </summary>
+  /// <param name="path"></param>
+  /// <returns></returns>
+  static public bool isLastFolderDotFolder(string path)
+  {
+    path = path.Replace("\\", "/"); // make path with all /
+
+    // remove all last /
+    while (path[path.Length - 1] == '/')
+    {
+      path.Substring(0, path.Length - 1);
+    }
+
+    string[] split = path.Split('/');
+    return split[split.Length - 1].StartsWith(".");
+  }
+
+  /// <summary>
+  /// removes file name and extension from given path
+  /// also replace \ by /
+  /// </summary>
+  /// <param name="path"></param>
+  /// <returns></returns>
+  static protected string cleanPathFromFile(string path)
+  {
+    path = path.Replace("\\", "/");
+
+    int extIndex = path.LastIndexOf('.'); // some folder have .
+    int lastSlash = path.LastIndexOf('/');
+
+    if (extIndex > 0)
+    {
+      path = path.Substring(0, lastSlash); // remove any file name
+    }
+
+    if (path[path.Length - 1] == '/') path.Substring(0, path.Length - 1); // remove last /
+
+    return path;
+  }
+
+  public static void DeleteDirectory(string target_dir)
+  {
+    if (!Directory.Exists(target_dir)) return;
+
+    string[] files = Directory.GetFiles(target_dir);
+    string[] dirs = Directory.GetDirectories(target_dir);
+
+    foreach (string file in files)
+    {
+      File.SetAttributes(file, FileAttributes.Normal);
+      File.Delete(file);
+    }
+
+    foreach (string dir in dirs)
+    {
+      DeleteDirectory(dir);
+    }
+
+    Directory.Delete(target_dir, false);
+  }
+}
