ceaeb8993f25c4acd21b726e4583a6ebbdc86a3e
ab <andre.berlemont@gmail.com>
2020-04-10 16:13:53 +0200

    scenes profil, scriptable fetcher

---
+++ Editor/DataBuildSettingProfilScenes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+/// <summary>
+/// 
+/// https://docs.unity3d.com/ScriptReference/EditorBuildSettingsScene.html
+/// 
+/// </summary>
+
+[CreateAssetMenu(menuName = "protoss/create DataBuildSettingProfilScenes", order = 100)]
+public class DataBuildSettingProfilScenes : ScriptableObject
+{
+  public string[] build_settings_scenes_paths;
+
+#if UNITY_EDITOR
+  [ContextMenu("apply")]
+  public void apply()
+  {
+    List<EditorBuildSettingsScene> tmp = new List<EditorBuildSettingsScene>();
+
+    foreach(string path in build_settings_scenes_paths)
+    {
+      tmp.Add(new EditorBuildSettingsScene(path, true));
+    }
+
+    EditorBuildSettings.scenes = tmp.ToArray();
+  }
+
+  [ContextMenu("record")]
+  public void record()
+  {
+    List<string> tmp = new List<string>();
+
+    EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+    foreach(EditorBuildSettingsScene sc in scenes)
+    {
+      Debug.Log(sc.path);
+      tmp.Add(sc.path);
+    }
+    build_settings_scenes_paths = tmp.ToArray();
+
+    EditorUtility.SetDirty(this);
+  }
+#endif
+
+}
---
+++ Halpers/HalperScriptables.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/*
+[CreateAssetMenu(menuName = "protoss/create DataClass", order = 100)]
+public class DataClass : ScriptableObject
+{
+}
+*/
+
+static public class HalperScriptables {
+
+#if UNITY_EDITOR
+  static public T getScriptableObjectInEditor<T>(string nameEnd = "") where T : ScriptableObject
+  {
+    string[] all = AssetDatabase.FindAssets("t:"+typeof(T).Name);
+    for (int i = 0; i < all.Length; i++)
+    {
+      Object obj = AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(all[i]), typeof(T));
+      T data = obj as T;
+
+      if (data == null) continue;
+      if(nameEnd.Length > 0)
+      {
+        if (!data.name.EndsWith(nameEnd)) continue;
+      }
+
+      return data;
+    }
+    Debug.LogWarning("can't locate scriptable of type " + typeof(T).Name + " (filter name ? " + nameEnd + ")");
+    return null;
+  }
+#endif
+
+}
